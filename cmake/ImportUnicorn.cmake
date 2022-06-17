b921f0f5c09b233e0b63458756413a024d78961d
Eyck Jentzsch <eyck@minres.com>
2022-06-17 11:25:07 +0200

    fixes build system wrt external projects

--- cmake/ImportUnicorn.cmake
+++ cmake/ImportUnicorn.cmake
@@ -6,21 +6,31 @@
 
 option(UPDATE_UNICORN_SRC "Always update the unicorn engine library" OFF)
 
-include(ExternalProject)
-ExternalProject_Add(
-	unicorn
+include(FetchContent)
+FetchContent_Declare(
+ 	unicorn_git
     GIT_REPOSITORY "https://github.com/unicorn-engine/unicorn.git"
     GIT_TAG        "origin/master"
     GIT_SHALLOW    ON
   	UPDATE_DISCONNECTED NOT ${UPDATE_UNICORN_SRC} # When enabled, this option causes the update step to be skipped.
-  	CMAKE_ARGS    -DCMAKE_INSTALL_PREFIX:PATH=install -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 )
+FetchContent_Populate(unicorn_git)
+FetchContent_GetProperties(
+  unicorn_git
+  SOURCE_DIR unicorn_git_SRC_DIR
+  BINARY_DIR unicorn_git_BIN_DIR
+  POPULATED unicorn_git_FOUND
+)
+add_custom_command(
+	OUTPUT ${unicorn_git_BIN_DIR}/lib/libunicorn.so
+	COMMAND ${CMAKE_COMMAND}
+	ARGS -S ${unicorn_git_SRC_DIR} -B ${unicorn_git_SRC_DIR}/build -DCMAKE_INSTALL_PREFIX=${unicorn_git_BIN_DIR} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+	COMMAND ${CMAKE_COMMAND}
+	ARGS --build ${unicorn_git_SRC_DIR}/build --target install
+	DEPENDS ${unicorn_git_SRC_DIR})
+add_custom_target(unicorn_build  DEPENDS ${unicorn_git_BIN_DIR}/lib/libunicorn.so)
 
-ExternalProject_Get_Property(unicorn install_dir)
-message(STATUS "Unicorn is ${install_dir}")
-set(unicorn_ROOT ${install_dir}/src/unicorn)
-set(unicorn_INSTALL ${install_dir}/src/unicorn-build/install)
-add_library (libunicorn SHARED IMPORTED)
-add_dependencies(libunicorn unicorn)
-set_target_properties(libunicorn PROPERTIES IMPORTED_LOCATION ${unicorn_INSTALL}/lib/libunicorn.so)
-set_target_properties(libunicorn PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${unicorn_INSTALL}/include)
+add_library (unicorn INTERFACE) #SHARED IMPORTED)
+add_dependencies(unicorn unicorn_build)
+target_include_directories(unicorn INTERFACE ${unicorn_git_BIN_DIR}/include)
+target_link_libraries(unicorn INTERFACE ${unicorn_git_BIN_DIR}/lib/libunicorn.so)
