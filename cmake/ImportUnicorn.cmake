893864178f65d5632ddbaa8b0a625d83507d0bfc
Eyck Jentzsch <eyck@minres.com>
2022-08-06 11:33:54 +0200

    updates unicorn version to 2.0.0

--- cmake/ImportUnicorn.cmake
+++ cmake/ImportUnicorn.cmake
@@ -10,7 +10,7 @@ include(FetchContent)
 FetchContent_Declare(
  	unicorn_git
     GIT_REPOSITORY "https://github.com/unicorn-engine/unicorn.git"
-    GIT_TAG        "origin/master"
+    GIT_TAG        "2.0.0"
     GIT_SHALLOW    ON
   	UPDATE_DISCONNECTED NOT ${UPDATE_UNICORN_SRC} # When enabled, this option causes the update step to be skipped.
 )
@@ -26,7 +26,7 @@ add_custom_command(
 	COMMAND ${CMAKE_COMMAND}
 	ARGS -S ${unicorn_git_SRC_DIR} -B ${unicorn_git_SRC_DIR}/build -DCMAKE_INSTALL_PREFIX=${unicorn_git_BIN_DIR} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 	COMMAND ${CMAKE_COMMAND}
-	ARGS --build ${unicorn_git_SRC_DIR}/build --target install
+	ARGS --build ${unicorn_git_SRC_DIR}/build --target install --parallel 
 	DEPENDS ${unicorn_git_SRC_DIR})
 add_custom_target(unicorn_build  DEPENDS ${unicorn_git_BIN_DIR}/lib/libunicorn.so)
 
